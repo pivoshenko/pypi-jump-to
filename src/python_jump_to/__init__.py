ec4ec90ad24756aabc10cb146ba82112b4a198d4
Volodymyr Pivoshenko <volodymyr.pivoshenko@gmail.com>
2022-12-04 18:40:53 +0000

    chore: init

---
+++ src/python_jump_to/__init__.py
@@ -0,0 +1,13 @@
+"""python-jump-to - a quick navigation tool for the PyPI packages."""
+
+__title__ = "python-jump-to"
+__summary__ = "python-jump-to - a quick navigation tool for the PyPI packages."
+__uri__ = "https://github.com/volopivoshenko/pjt"
+
+__version__ = "0.0.0"
+
+__author__ = "Volodymyr Pivoshenko"
+__email__ = "volodymyr.pivoshenko@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright 2022, Volodymyr Pivoshenko"
